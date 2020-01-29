6af3d5b8e3bf22c624638b55d5169b2f0f117dac
Francesco Garofalo <francescogarofalo182@live.com>
2020-01-29 17:44:36 +0100

    Upload Project
    Upload entire project

---
+++ Assets/Menu/Script/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public Button cardboard;
+    public Button Nocardboard;
+    public GameObject primoMenu;
+    public GameObject cardboard2;
+
+
+	// Use this for initialization
+	void Start () {
+        cardboard.GetComponent<Button>();
+        Nocardboard.GetComponent<Button>();
+
+        cardboard.onClick.AddListener(EseguiCardboard);
+        Nocardboard.onClick.AddListener(EseguiNoCardboard);
+	}
+	
+    public void EseguiCardboard()
+    {
+        primoMenu.SetActive(false);
+        cardboard2.SetActive(true);
+        Debug.Log("MENU: Clicco Esegui Cardboard");
+        Main.attivaCardboard();
+    }
+
+    public void EseguiNoCardboard()
+    {
+        primoMenu.SetActive(false);
+        Debug.Log("MENU: Clicco Esegui no Cardboard");
+        Main.attivaNoCardboard();
+    }
+
+
+
+}
